ab108fae8176a5aceffba1076eb1abdd3c240e4e
lexStack3 <alexanderedim80@gmail.com>
2025-07-31 05:48:25 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list_books/', views.list_books, name='list_books'),
+    path('books/', views.list_books, name='list-books'),
+    path('library/<int:pk>', views.LibraryDetailView.as_view(),
+         name='library-detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
+
 
 def list_books(request):
     books = Book.objects.all()
     context = {
         'books': books
     }
-
     return render(request, 'relationship_app/list_books.html', context)
 
 
-class ListBooksView(ListView):
+class LibraryDetailView(DetailView):
     """Displays details for a specific library,
     listing all books available in the library."""
-    model = Book
-    template_name = 'relationship_app/'
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_queryset(self):
+        """Will use <pk> from the URL to prefetch all related <books>."""
+        return Library.objects.prefetch_related('books')
