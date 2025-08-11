5ed5f8c5dfe2050f07307e027d8edc7e0d665360
lexStack3 <alexanderedim80@gmail.com>
2025-08-11 07:40:07 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Book
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 
-@admin.register(User)
-class CustomUserAdminModel(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
     list_display = ('username', 'email', 'first_name',
                     'last_name', 'date_of_birth', 'is_active')
     list_filter = ('is_active', 'is_superuser', 'is_staff', 'groups')
@@ -31,3 +33,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author', 'pulication_year')
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
