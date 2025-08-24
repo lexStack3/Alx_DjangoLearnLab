4b614b4404f7ffaafe9d261cab9d0850b0ecfc89
lexStack3 <alexanderedim80@gmail.com>
2025-08-25 00:46:26 +0100

    Add a ViewSet view which support all CRUD operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', views.BookViewSet, basename='book_all')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('books/', views.BookList.as_view(), name='book-list')
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework import viewsets
 
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
