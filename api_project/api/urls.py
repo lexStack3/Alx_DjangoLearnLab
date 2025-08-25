26d2d4f76de1a431c6d202729bffe5b0dca22fad
lexStack3 <alexanderedim80@gmail.com>
2025-08-25 01:40:13 +0100

    Implementing Authentication and Permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books_all', views.BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('books/', views.BookList.as_view(), name='book-list')
+    path('books/', views.BookList.as_view(), name='book-list'),
+    path('token/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import SAFE_METHODS, IsAdminUser, IsAuthenticated
+from .permissions import ReadOnly
 
 
 class BookList(generics.ListAPIView):
@@ -13,3 +15,8 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [ReadOnly()]
+        return [IsAuthenticated(), IsAdminUser()]
