c66b00658624d63a4aabe9b634276e92f5ad502a
lexStack3 <alexanderedim80@gmail.com>
2025-08-28 15:02:05 +0100

    Setup and Project Configuration for Blog

---
+++ django_blog/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} - {self.content} by {self.author.username}"
