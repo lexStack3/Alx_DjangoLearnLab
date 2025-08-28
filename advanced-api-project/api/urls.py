c5e6952057d14089157bf7a236b3b99ae69d6f00
lexStack3 <alexanderedim80@gmail.com>
2025-08-28 12:55:04 +0100

    Updated the url routers

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
@@ -8,6 +9,7 @@ router.register(r'authors', views.AuthorAPIView, basename='author')
 router.register(r'books', views.BookAPIView, basename='book')
 
 urlpatterns = [
+    path('token/', obtain_auth_token, name='obtain_auth_token'),
     path('books/list/', views.ListView.as_view(), name='books-list'),
     path('books/detail/<int:pk>/', views.DetailView.as_view(), name='book-detial'),
     path('books/create/', views.CreateView.as_view(), name='book-create'),
